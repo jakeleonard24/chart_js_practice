13a787bab99880d22abc223cc7f19f066c2df99b
Jake Leonard <jake24leonard@gmail.com>
2017-12-13 10:22:32 -0700

    few changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import BarGraph from './Components/Bar'
 import Donut from './Components/Doughnut'
-import {Line} from 'react-chartjs-2';
+import LineGraph from './Components/Line'
+
 import {Bar} from 'react-chartjs-2';
 
 
@@ -18,32 +19,7 @@ class App extends Component {
       
 
       
-      data3: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [
-          {
-            label: 'Naps Per Month',
-            fill: false,
-            lineTension: 0.1,
-            backgroundColor: 'rgba(75,192,192,0.4)',
-            borderColor: 'rgba(75,192,192,1)',
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: 'rgba(75,192,192,1)',
-            pointBackgroundColor: '#fff',
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-            pointHoverBorderColor: 'rgba(220,220,220,1)',
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: [65, 59, 80, 81, 56, 55, 40]
-          }
-        ]
-      },
+      
 
       data4:{
         labels: [],
@@ -251,7 +227,7 @@ class App extends Component {
         
 </div>
       <div className='sizer'>
-        <Line data={this.state.data3} />
+        <LineGraph />
       </div>
 
       <div>
